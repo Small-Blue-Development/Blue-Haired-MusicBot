e41c2a7cf2a34ea56f72d4ee627fdb41b0c82d44
Azrael747 <saucy.speedballer@gmail.com>
2021-01-14 20:04:23 -0500

    Music bot 1.0.0

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'Nzk5MzU4OTA2NDc5NDExMjgx.YACa_g.kVUidmF7y1xQH4g6WeeIgkYQjRI',
+        prefix: '--',
+        //activity: 'ACTIVITY',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
